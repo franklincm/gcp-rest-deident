cc7f2f7bf5bff3427810ff7123297b5518451c60
Chase Franklin <chasefranklin@Chases-MBP.attlocal.net>
2021-05-02 11:05:30 -0500

    init

---
+++ src/main/java/io/nuvalence/onboarding/gcp/RESTDeident/RestDeidentApplication.java
@@ -0,0 +1,13 @@
+package io.nuvalence.onboarding.gcp.RESTDeident;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDeidentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestDeidentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/nuvalence/onboarding/gcp/RESTDeident/RestDeidentApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nuvalence.onboarding.gcp.RESTDeident;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestDeidentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
